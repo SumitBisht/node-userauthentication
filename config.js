e1d8513707082eb3a471a314ee26e053f6f8f8b1
sumitbisht <sumitbisht1987@gmail.com>
2014-01-18 13:23:53 +0530

    added template for email values to be inserted

--- config.js
+++ config.js
@@ -6,3 +6,13 @@
 exports.db = 'mongodb';
 exports.dbUrl = 'mongodb://127.0.0.1/test';
 exports.dbCollection = 'nodetest-users';
+
+// email settings (same as nodemailer)
+exports.emailType = 'Stub';
+exports.emailSettings = {
+  service: 'none',
+  auth: {
+    user: 'none',
+    pass: 'none'
+  }
+};
