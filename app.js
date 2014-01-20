1d9b539b497e418410ec1940241904e0c0c46fa4
sumitbisht <sumitbisht1987@gmail.com>
2014-01-20 18:54:26 +0530

    added view to look into user profile

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var config = require('./config.js');
 var lockit = require('lockit');
+var utls = require('lockit-utils');
+var moment = require('moment');
 
 var app = express();
+var adapter = require('lockit-' + config.db + '-adapter')(config);
 
 app.set('view engine', 'jade');
 app.configure(function(){
@@ -17,6 +20,26 @@ lockit(app, config);
 
 app.get('/', function(req, res){
 	res.render('index', { title: 'User authentication through Lockit' });
+});
+app.get('/users', function(req, res){
+	res.send("respond with a resource");
+});
+app.get('/profile', utls.restrict(config), function(req, res){
+	var email = req.session.email;
+	adapter.find('email', email, function(err, user){
+		if(err){
+			console.log('Unable to find profile details on the basis of email');
+			console.log(err);
+		}
+		var infos = {
+			title: 'Profile',
+			currentLoginIp: user.currentLoginIp,
+			failedLoginAttempts: req.session.failedLoginAttempts,
+			currentLoginTime: moment(user.currentLoginTime).format('ddd, MMM Do YYYY - HH:mm:ss'),
+			memberFor: moment(user.emailVerificationTimestamp).fromNow(true)
+		};
+		res.render('profile', infos);
+	});
 });
 
 app.use(app.router);
